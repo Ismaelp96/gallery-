3e5e8e600993863a6b63d871a64f61d8ad58f09f
Ismaelp96 <ismael6991@live.com>
2025-11-18 13:49:23 -0300

    fix: input-single-file.tsx

--- src/components/input-single-file.tsx
+++ src/components/input-single-file.tsx
@@ -1,58 +1,58 @@
-import { tv, type VariantProps } from 'tailwind-variants';
+import { tv, type VariantProps } from "tailwind-variants";
 
-import UploadFileIcon from '../assets/icons/upload-file.svg?react';
-import Icon from './icon';
-import Text from './text';
+import UploadFileIcon from "../assets/icons/upload-file.svg?react";
+import Icon from "./icon";
+import Text from "./text";
 
 export const inputSingleFileVariants = tv({
-	base: `
+  base: `
   flex flex-col items-center justify-center w-full
   border border-solid border-border-primary
-  group-houver:border-border-active
-  rounded-lg
+  group-hover:border-border-active
+  rounded-lg gap-1 transition
   `,
-	variants: {
-		size: {
-			md: 'px-5 py-6',
-		},
-	},
-	defaultVariants: {
-		size: 'md',
-	},
+  variants: {
+    size: {
+      md: "px-5 py-6",
+    },
+  },
+  defaultVariants: {
+    size: "md",
+  },
 });
 
 export const inputSingleFileIconVariants = tv({
-	base: 'fill-placeholder',
-	variants: {
-		size: {
-			md: 'w-8 h-8',
-		},
-	},
+  base: "fill-placeholder",
+  variants: {
+    size: {
+      md: "w-8 h-8",
+    },
+  },
 });
 
 interface InputSingleFileProps
-	extends VariantProps<typeof inputSingleFileVariants>,
-		Omit<React.ComponentProps<'input'>, 'size'> {}
+  extends VariantProps<typeof inputSingleFileVariants>,
+    Omit<React.ComponentProps<"input">, "size"> {}
 
 export default function InputSingleFile({ size }: InputSingleFileProps) {
-	return (
-		<div>
-			<div className='w-full relative group cursor-pointer'>
-				<input
-					type='file'
-					className={`absolute top-0 right-0 w-full h-full opacity-0 cursor-pointer`}
-				/>
-				<div className={inputSingleFileVariants({ size })}>
-					<Icon
-						svg={UploadFileIcon}
-						className={inputSingleFileIconVariants({ size })}
-					/>
-					<Text variant='label-medium' className='text-placeholder'>
-						Arraste o arquivo aqui
-						<br /> ou clique para selecionar
-					</Text>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div>
+      <div className="w-full relative group cursor-pointer">
+        <input
+          type="file"
+          className={`absolute top-0 right-0 w-full h-full opacity-0 cursor-pointer`}
+        />
+        <div className={inputSingleFileVariants({ size })}>
+          <Icon
+            svg={UploadFileIcon}
+            className={inputSingleFileIconVariants({ size })}
+          />
+          <Text variant="label-medium" className="text-placeholder text-center">
+            Arraste o arquivo aqui
+            <br /> ou clique para selecionar
+          </Text>
+        </div>
+      </div>
+    </div>
+  );
 }
