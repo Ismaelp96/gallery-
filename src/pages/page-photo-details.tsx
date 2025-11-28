9daa9eb6f034dcf0889022cd96b64d1e4ecda5cb
ismaelp96 <ismael6991@live.com>
2025-11-28 11:26:08 -0300

    feat: Mock da página de dtalhes da imagem

--- src/pages/page-photo-details.tsx
+++ src/pages/page-photo-details.tsx
@@ -1,11 +1,22 @@
-import { useParams } from 'react-router';
+import Container from '../components/container';
+import Skeleton from '../components/skeleton';
 import Text from '../components/text';
+import PhotosNavigator from '../contexts/photos/components/photos-navigator';
+import type { Photo } from '../contexts/photos/models/photo';
 
 export function PagePhotoDetails() {
-	const { id } = useParams();
+	const isLoadingPhoto = false;
+	const photo = {} as Photo;
 	return (
-		<>
-			<Text variant='heading-medium'>Id da foto: {id}</Text>
-		</>
+		<Container>
+			<header className='flex items-center justify-between gap-8 mb-8'>
+				{isLoadingPhoto ? (
+					<Text variant='heading-large'>{photo.title}</Text>
+				) : (
+					<Skeleton className='w-48 h8' />
+				)}
+				<PhotosNavigator />
+			</header>
+		</Container>
 	);
 }
